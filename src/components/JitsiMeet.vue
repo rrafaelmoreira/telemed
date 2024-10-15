60107f4797f27c131c5077816df33c20ecbb375c
rrafaelmoreira <moreira.rafael1@aluno.ifsp.edu.br>
2024-10-15 17:34:49 -0300

    Reset git index

---
+++ src/components/JitsiMeet.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="jitsi-container">
+      <iframe
+        :src="jitsiUrl"
+        allow="camera; microphone; fullscreen; display-capture"
+        style="width: 100%; height: 100%; border: 0;"
+      ></iframe>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'JitsiMeet',
+    data() {
+      return {
+        roomName: 'my-custom-room', // Nome da sala de videoconferência
+        domain: '35.196.247.119', // Domínio do servidor Jitsi
+      };
+    },
+    computed: {
+      jitsiUrl() {
+        return `https://${this.domain}/${this.roomName}`;
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .jitsi-container {
+    width: 80%; /* Defina a largura desejada */
+    height: 80vh; /* Defina a altura desejada */
+    margin: 0 auto; /* Centraliza o contêiner horizontalmente */
+    border: 1px solid #ccc; /* Adiciona uma borda para visualização */
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); /* Adiciona uma sombra para visualização */
+  }
+  </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,24 @@
 <template>
-    <div>
-      <h1>Home Page</h1>
-      <p>Bem-vindo à página inicial!</p>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'HomeView'
-  }
-  </script>
-  
-  <style scoped>
-  h1 {
-    color: #42b983;
-  }
-  </style>
+  <div>
+    <h1>Home Page</h1>
+    <p>Bem-vindo à página inicial!</p>
+    <JitsiMeet />
+  </div>
+</template>
+
+<script>
+import JitsiMeet from '../components/JitsiMeet.vue';
+
+export default {
+  name: 'HomeView',
+  components: {
+    JitsiMeet,
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
